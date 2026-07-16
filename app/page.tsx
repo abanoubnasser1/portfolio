fd51adeea9fc0e9e909dd44bf8894e8c242cde05
abanoubnasser1 <abanob.nasser7@gmail.com>
2026-07-16 17:19:45 +0300

    back to top

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
       <About />
       <Projects />
       <Contact />
-      <section id="back to top" className="mx-auto w-full flex justify-center max-w-[1700px] py-10 ">
+      <section id="back to top" className="mx-auto w-full flex justify-center max-w-[1700px] py-30 ">
               <div className="text-[10px] font-semibold uppercase tracking-[0.25em]">
          <a href="#" className="transition hover:text-white">
           back to top
