a6c90196e6e122fa1132170f44f90e324cc27992
abanoubnasser1 <abanob.nasser7@gmail.com>
2026-07-18 13:37:09 +0300

    fix logos

--- components/clients/LogoMarquee.tsx
+++ components/clients/LogoMarquee.tsx
@@ -1,32 +1,32 @@
 "use client";
 
 const logos = [
-  "/content/clients/logo (1).png",
-  "/content/clients/logo (2).png",
-  "/content/clients/logo (3).png",
-  "/content/clients/logo (4).png",
-  "/content/clients/logo (5).png",
-  "/content/clients/logo (6).png",
-    "/content/clients/logo (7).png",
+
+    "/content/clients/logo (1).png",
+    "/content/clients/logo (2).png",
+    "/content/clients/logo (4).png",
+    "/content/clients/logo (5).png",
+    "/content/clients/logo (6).png",
     "/content/clients/logo (8).png",
     "/content/clients/logo (9).png",
     "/content/clients/logo (10).png",
     "/content/clients/logo (11).png",
+    "/content/clients/logo (31).png",
     "/content/clients/logo (12).png",
     "/content/clients/logo (13).png",
-    "/content/clients/logo (14).png",
     "/content/clients/logo (15).png",
-    "/content/clients/logo (16).png",
     "/content/clients/logo (17).png",
     "/content/clients/logo (18).png",
-    "/content/clients/logo (19).png",
-    "/content/clients/logo (20).png",
-    "/content/clients/logo (21).png",
-    "/content/clients/logo (22).png",
-    "/content/clients/logo (23).png",
-    "/content/clients/logo (24).png",
     "/content/clients/logo (25).png",
-    "/content/clients/logo (26).png",
+    "/content/clients/logo (27).png",
+    "/content/clients/logo (28).png",
+    "/content/clients/logo (29).png",
+    "/content/clients/logo (30).png",
+
+
+
+
+   
 ];
 
 export default function LogoMarquee() {
